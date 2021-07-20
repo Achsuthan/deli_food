a39e2d11f4683706a6091805116ffeced1a710f8
Achsuthan <achsuthan@icloud.com>
2021-07-20 12:36:13 +0800

    size bug fixed

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -41,94 +41,99 @@ class _RegisterScreenState extends State<RegisterScreen> {
             )),
       ),
       backgroundColor: Colors.red,
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: 20,
-            right: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              LogoWithText(),
-              SizedBox(
-                height: 40,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
               ),
-              Text(
-                "SIGN UP",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              DeliTextFiled(
-                hintLabel: "Username",
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              DeliTextFiled(
-                hintLabel: "Email",
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              DeliPasswordTextFiled(
-                isVisible: isVisible,
-                handleButton: () => handleShowHidePassword(),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: DeliBorderButton(),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "By registering, you aggree to the Deli Food ",
-                      ),
-                      TextSpan(
-                        text: "Terms and condition",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print("Terms and conditon");
-                          },
-                      ),
-                      TextSpan(
-                        text: " and ",
-                      ),
-                      TextSpan(
-                        text: "Privacy Policy",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            print("Privacy Policy");
-                          },
-                      ),
-                    ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  LogoWithText(),
+                  SizedBox(
+                    height: 40,
                   ),
-                ),
+                  Text(
+                    "SIGN UP",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  DeliTextFiled(
+                    hintLabel: "Username",
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  DeliTextFiled(
+                    hintLabel: "Email",
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  DeliPasswordTextFiled(
+                    isVisible: isVisible,
+                    handleButton: () => handleShowHidePassword(),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: DeliBorderButton(),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text:
+                                "By registering, you aggree to the Deli Food ",
+                          ),
+                          TextSpan(
+                            text: "Terms and condition",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Terms and conditon");
+                              },
+                          ),
+                          TextSpan(
+                            text: " and ",
+                          ),
+                          TextSpan(
+                            text: "Privacy Policy",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Privacy Policy");
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
